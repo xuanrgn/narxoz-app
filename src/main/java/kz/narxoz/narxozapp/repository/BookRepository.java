38dcba7e18e83e3f3b2f49ff49dabc3c3ad0d4a5
Argyn Khuan <laxeyo7760@vsooc.com>
2022-04-08 09:11:15 +0600

    Revert "Add lombok, Add Book"
    This reverts commit 6f6d0c815fbffe352af0cb9524ed01c16ab2577d.

--- src/main/java/kz/narxoz/narxozapp/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package kz.narxoz.narxozapp.repository;
-
-import kz.narxoz.narxozapp.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
