19c56a22a415167b10015c1706fe398113014569
Argyn Khuan <laxeyo7760@vsooc.com>
2022-04-08 09:51:53 +0600

    Revert "Add Lombok, Book Table"
    This reverts commit e427898650085db736fba68b4273c76b243d8e75.

--- src/main/java/kz/narxoz/narxozapp/model/User.java
+++ src/main/java/kz/narxoz/narxozapp/model/User.java
@@ -1,18 +1,9 @@
 package kz.narxoz.narxozapp.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "users")
 public class User {
 
@@ -21,9 +12,50 @@ public class User {
     private long id;
 
     private String name;
-
     private String surname;
-
     private String email;
 
+
+
+    public User() {
+    }
+
+    public User(int id, String name, String surname, String email) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
